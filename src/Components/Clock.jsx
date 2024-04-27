a85da0ab42fac3293602749ec2a5b0ac017b86cb
rakibprog <contact.engrakib@gmail.com>
2024-04-27 22:09:15 +0600

    set state

--- src/Components/Clock.jsx
+++ src/Components/Clock.jsx
@@ -4,11 +4,26 @@ import React from "react";
 
 class Clock extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.state = { date: new Date() }
+    }
+
+    componentDidMount() {
+        setInterval( () => {
+
+            this.setState({
+                date: new Date(),
+            });
+
+        },1000);
+    }
+
     render() {
 
         return (
             <h1 className="Heading">
-                <span className="text"> { new Date().toLocaleTimeString('bn-BD') } </span>
+                <span className="text"> { this.state.date.toLocaleTimeString('bn-BD') } </span>
             </h1>
         )
     }
